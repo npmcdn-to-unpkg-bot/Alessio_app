de99e2188df459a0c7154e10f72f5e5ac8c97355
Alessio Prestileo <alessioprestileo@gmail.com>
2016-06-16 16:18:37 +0200

    fixed dependencies problems in unit tests; added subfolders to 'app' folder

--- app/main.ts
+++ app/main.ts
@@ -3,13 +3,13 @@ import { provide }    from '@angular/core';
 import { XHRBackend } from '@angular/http';
 
 import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
-import { ServerData }               	 from './server-data';
+import { ServerData }               	 from './utils/server-data';
 
 // The usual bootstrapping imports
 import { bootstrap }      from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
 
-import { AppComponent }   from './app.component';
+import { AppComponent }   from './components/app.component';
 
 bootstrap(AppComponent, [
   HTTP_PROVIDERS,
--- unit_tests/app.component.spec.ts
+++ unit_tests/app.component.spec.ts
@@ -12,12 +12,22 @@ import { provide }        			from '../node_modules/@angular/core';
 import { ViewMetadata }   			from '../node_modules/@angular/core';
 import { PromiseWrapper } 			from '../node_modules/@angular/core/src/facade/promise';
 
-import { ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '../node_modules/@angular/router-deprecated';
-import { AppComponent } from '../app/app.component';
-import { ServerService } from '../app/server.service'
-import { DepartmentsService } from '../app/departments.service';
-import { ServerServiceMock } from '../app/server.service.mock'
-import { DepartmentsServiceMock } from '../app/departments.service.mock';
+import { Directive } from '../node_modules/@angular/core';
+import { RouterLink, RouterOutlet, ROUTER_PROVIDERS } from '../node_modules/@angular/router-deprecated';
+import { AppComponent } from '../app/components/app.component';
+import { ServerService } from '../app/services/server.service'
+import { DepartmentsService } from '../app/services/departments.service';
+import { ServerServiceMock } from '../app/services/server.service.mock'
+import { DepartmentsServiceMock } from '../app/services/departments.service.mock';
+
+////////  LOCAL MOCKS  /////////////
+@Directive({
+	selector: '[routerLink]',
+  inputs: ['routeParams: routerLink', 'target: target'],
+})
+class RouterLinkMock {}
+@Directive({ selector: 'router-outlet' })
+class RouterOutletMock {}
 
 ////////  SPECS  /////////////
 describe('AppComponent with new', function () {
@@ -37,7 +47,8 @@ describe('AppComponent with DI', function () {
 		DepartmentsService],
 		(appComponent: AppComponent) => {
 	    expect(appComponent).toBeDefined();
-	}));
+		})
+	);
 });
 describe('AppComponent with TCB', function() {
 
@@ -45,10 +56,28 @@ describe('AppComponent with TCB', function() {
     async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
 			tcb
 				.overrideProviders(AppComponent, [
+					ROUTER_PROVIDERS,
 					provide(ServerService, {useClass: ServerServiceMock}), 
 					provide(DepartmentsService, {useClass: DepartmentsServiceMock})])
+				.overrideDirective(AppComponent, RouterLink, RouterLinkMock)
+				.overrideDirective(AppComponent, RouterOutlet, RouterOutletMock)
 				.createAsync(AppComponent).then(fixture => {
 					expect(true).toBe(true);
+				});
+	})));
+  it('should have the title "Alessio\'s warehouse"',
+    async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+			tcb
+				.overrideProviders(AppComponent, [
+					ROUTER_PROVIDERS,
+					provide(ServerService, {useClass: ServerServiceMock}), 
+					provide(DepartmentsService, {useClass: DepartmentsServiceMock})])
+				.overrideDirective(AppComponent, RouterLink, RouterLinkMock)
+				.overrideDirective(AppComponent, RouterOutlet, RouterOutletMock)
+				.createAsync(AppComponent).then(fixture => {
+					fixture.detectChanges();
+          let h1 = fixture.debugElement.query(By.css('h1')).nativeElement;
+      		expect(h1.innerText).toMatch('Alessio\'s warehouse');
 				});
 		})));
 });
